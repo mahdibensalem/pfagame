1628430d0f7b68200a713b4ee0a5a54e6ee67e38
hamzahassen6 <73426280+hamzahassen6@users.noreply.github.com>
2020-10-30 00:54:15 +0100

    adding colors to prefab
    adding colors to make work easier while giving the scripts of the role

---
+++ pfa_game/Assets/colorful.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class colorful : MonoBehaviour
+{
+    Renderer Rend;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rend= GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        Debug.Log(Rend.material.color);
+        
+    }
+}
