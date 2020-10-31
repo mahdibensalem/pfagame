3a5e4c848e19ce71f8f012fcb158e537678fdbc6
hamzahassen6 <73426280+hamzahassen6@users.noreply.github.com>
2020-10-31 17:04:31 +0100

    adding some scripts but mvtsphere not working
    camm follow work
    colorful  still not writed yet
    mvt sphere not working

---
+++ pfa_game/Assets/cam_follow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cam_follow : MonoBehaviour
+{
+    public Transform player;
+    public float smoothspeed=0.125f;
+    public Vector3 cam_settings;
+
+    private void FixedUpdate()
+
+    {
+        Vector3 last_position= player.position + cam_settings;
+        Vector3 smoothedposition = Vector3.Lerp(transform.position, last_position, smoothspeed);
+
+        transform.position = smoothedposition ;
+        transform.LookAt(player);
+    }
+
+}
